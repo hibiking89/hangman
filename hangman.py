9b6f70d82fa7c100d9bc02a09e7cd301b5314a9a
Hibiking89 <hibikon.asr.89@gmail.com>
2019-05-17 20:46:56 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,45 @@
+# http://tinyurl.com/h9q2cpc
+
+import random
+
+def hangman():
+    word_list = ["japan", "korea", "italy"]
+    random_number = random.randint(0,2)
+    word = word_list[random_number]
+    wrong = 0
+    stages = ["",
+              "______      ",
+              "|           ",
+              "|     |     ",
+              "|     O     ",
+              "|    /|\    ",
+              "|    / \    ",
+              "|           "
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してください"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け！正解は {}.".format(word))
+
+hangman()
